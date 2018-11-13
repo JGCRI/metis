52bdb792df262470028d4562bdb92adafc6c613d
zarrar <zarrar85@gmail.com>
2018-11-13 14:50:19 -0500

    Updated srn.ChartProcess and srn.readgcam to be more robust

--- R/srn.readgcam.R
+++ R/srn.readgcam.R
@@ -15,7 +15,18 @@
 #' with full path is provided otherwise it will search for a dataProj.proj file in the existing
 #' folder which may have been created from an old run.
 #' @param dataProj Optional. A default 'dataProj.proj' is produced if no .Proj file is specified.
-#' @param regions The regions to analyze in a vector. Example c('Colombia','Pakistan')
+#' @param regionsSelect The regions to analyze in a vector. Example c('Colombia','Argentina')
+#' @param queriesSelect Default = "All". Vector of queries to read from the queryxml for example
+#' c("Total final energy by aggregate end-use sector", "Population by region"). The queries must be
+#' availble in the queryxml file. Current list of available paramaters are:
+#' \itemize{
+#' \item "Total final energy by aggregate end-use sector"
+#' \item "GDP per capita MER by region" : Where MER is "Market Exchange Rate"
+#' \item "GDP MER by region" : Where MER is "Market Exchange Rate"
+#' \item "GDP Growth Rate (Percent)" : Calculated based on the GDP MER by region.
+#' \item "Population by region"
+#' \item "ag production by tech" : Where technologies signify irrigated or rainfed
+#' }
 #' @return A list with the scenarios in the gcam database, queries in the queryxml file and a
 #' tibble with gcam data formatted for srn charts.
 #' @keywords gcam, gcam database, query
@@ -25,7 +36,7 @@
 srn.readgcam <- function(gcamdatabasePath, gcamdatabaseName, queryxml = "srnQueries.xml",
                          scenOrigNames, scenNewNames = NULL,
                          reReadData = T, dataProj = "dataProj.proj", dirOutputs = paste(getwd(), "/outputs", sep = ""),
-                         regions = NULL) {
+                         regionsSelect = NULL, queriesSelect="All") {
 
 #------------------
 # Load required Libraries
@@ -42,36 +53,19 @@ requireNamespace("dplyr",quietly = T)
     class1 -> connx -> aggregate -> Units -> sources
 
     # Create necessary directories if they dont exist.
-    if (!dir.exists(dirOutputs))
-        {
-            dir.create(dirOutputs)
-        }  # Output Directory
-    if (is.null(regions)) {
-        if (!dir.exists(paste(dirOutputs, "/AllRegions", sep = "")))
-            {
-                dir.create(paste(dirOutputs, "/AllRegions", sep = ""))
-            }  # GCAM output directory
-        if (!dir.exists(paste(dirOutputs, "/AllRegions/regional", sep = "")))
-            {
-                dir.create(paste(dirOutputs, "/AllRegions/regional", sep = ""))
-            }  # GCAM output directory
-    } else {
-        for (region_i in regions) {
-            if (!dir.exists(paste(dirOutputs, "/", region_i, sep = "")))
-                {
-                  dir.create(paste(dirOutputs, "/", region_i, sep = ""))
-                }  # GCAM output directory
-            if (!dir.exists(paste(dirOutputs, "/", region_i, "/regional", sep = "")))
-                {
-                  dir.create(paste(dirOutputs, "/", region_i, "/regional", sep = ""))
-                }  # GCAM output directory
-        }
-    }
+    if (!dir.exists(dirOutputs)){
+      dir.create(dirOutputs)}  # Output Directory
+    if (!dir.exists(paste(dirOutputs, "/Tables_gcam", sep = ""))){
+        dir.create(paste(dirOutputs, "/Tables_gcam", sep = ""))}  # GCAM output directory
+    if (!dir.exists(paste(dirOutputs, "/Tables_Templates", sep = ""))){
+         dir.create(paste(dirOutputs, "/Tables_Templates", sep = ""))}  # GCAM output directory
+    if (!dir.exists(paste(dirOutputs, "/Tables_Local", sep = ""))){
+         dir.create(paste(dirOutputs, "/Tables_Local", sep = ""))}  # GCAM output directory
+
 
     # Check for new scenario names
     if (is.null(scenNewNames)) {
-        scenNewNames <- scenOrigNames
-    }
+      scenNewNames <- scenOrigNames}
 
     # Read gcam database or existing dataProj.proj
     if (!reReadData) {
@@ -81,16 +75,14 @@ requireNamespace("dplyr",quietly = T)
             stop(paste("No ", dataProj, " file exists. Please set reReadData=T to create dataProj.proj"))
         }
     } else {
-        if (file.exists(dataProj))
-            {
-                file.remove(dataProj)
-            }  # Delete old project file
+        if (file.exists(dataProj)){
+                file.remove(dataProj)}  # Delete old project file
         for (scenario_i in scenOrigNames) {
             dataProj.proj <- addScenario(conn = localDBConn(gcamdatabasePath, gcamdatabaseName), proj = dataProj,
                 scenario = scenario_i, queryFile = paste(gcamdatabasePath, "/", queryxml, sep = ""))  # Check your queries file
         }
         file.copy(from = paste(getwd(), "/", dataProj, sep = ""), to = gcamdatabasePath, overwrite = T,
-            copy.mode = TRUE)
+                  copy.mode = TRUE)
         file.remove(dataProj)
         dataProjLoaded <- loadProject(paste(gcamdatabasePath, "/", dataProj, sep = ""))
     }
@@ -100,15 +92,24 @@ requireNamespace("dplyr",quietly = T)
     queries <- listQueries(dataProjLoaded)  # List of Queries in queryxml
 
 
-    # Read in paramaters from query file and format for later use
-    data <- tibble()
+    # Read in paramaters from query file to create formatted table
+    datax <- tibble()
+
+    if(queriesSelect=="All"){queriesx <- queries} else{
+      if(!all(queriesSelect %in% queries)){stop("No parameters are available in queryxml.
+                               Please check your queriesSelect entries or your queryxml")} else {
+      if(length(queriesSelect[!(queriesSelect %in% queries)])>0){
+        print(paste("Parameters not available in queryxml: ", paste(queriesSelect[!(queriesSelect %in% queries)],collapse=", "), sep=""))
+        print(paste("Running remaining queriesSelect: ",  paste(queriesSelect[(queriesSelect %in% queries)],collapse=", "), sep=""))}
+      queriesx <- queriesSelect}
+    }
 
     # Total final energy by aggregate end-use sector
     paramx <- "Total final energy by aggregate end-use sector"
-    if (paramx %in% queries) {
+    if (paramx %in% queriesx) {
         tbl <- getQuery(dataProjLoaded, paramx)  # Tibble
-        if (!is.null(regions)) {
-            tbl <- tbl %>% dplyr::filter(region %in% regions)
+        if (!is.null(regionsSelect)) {
+            tbl <- tbl %>% dplyr::filter(region %in% regionsSelect)
         }
         tbl <- tbl %>%
           left_join(data_frame(scenOrigNames, scenNewNames), by = c(scenario = "scenOrigNames")) %>%
@@ -135,17 +136,17 @@ requireNamespace("dplyr",quietly = T)
           dplyr::select(origScen,origQuery, origValue, origUnits, origX, region, param, scenario,
                         value, units, vintage, x, xLabel, aggregate, class1, classLabel1, classPalette1,
                         class2, classLabel2, classPalette2)
-        data <- bind_rows(data, tbl)
+        datax <- bind_rows(datax, tbl)
     } else {
         print(paste("Paramater '", paramx, "' not found in database", sep = ""))
     }
 
     # GDP MER by region
     paramx <- "GDP MER by region"
-    if (paramx %in% queries) {
+    if (paramx %in% queriesx) {
         tbl <- getQuery(dataProjLoaded, paramx)  # Tibble
-        if (!is.null(regions)) {
-            tbl <- tbl %>% dplyr::filter(region %in% regions)
+        if (!is.null(regionsSelect)) {
+            tbl <- tbl %>% dplyr::filter(region %in% regionsSelect)
         }
         tbl <- tbl %>%
           left_join(data_frame(scenOrigNames, scenNewNames), by = c(scenario = "scenOrigNames")) %>%
@@ -172,7 +173,7 @@ requireNamespace("dplyr",quietly = T)
           dplyr::select(scenario, region, param, sources, class1, class2, x, xLabel, vintage, units, value,
                         aggregate, classLabel1, classPalette1,classLabel2, classPalette2,
                         origScen, origQuery, origValue, origUnits, origX)
-        data <- bind_rows(data, tbl)
+        datax <- bind_rows(datax, tbl)
     } else {
         print(paste("Paramater '", paramx, "' not found in database", sep = ""))
     }
@@ -182,33 +183,39 @@ requireNamespace("dplyr",quietly = T)
     # Create Data Template
     #---------------------
 
-    dataTemplate <- data %>%
-      mutate(scenario = "Local Data", origScen = "Local Data", value = 0, origValue = 0, sources="Sources") %>%
-      unique() %>%
-      dplyr::select(scenario, region, sources, param, x, xLabel, vintage, class1, class2, units, value, aggregate,
-                    classLabel1, classPalette1, classLabel2, classPalette2)
+    dataTemplate <- datax %>%
+      mutate(scenario = "Local Data", value = 0, sources="Sources", x=2010, vintage="vintage if available") %>%
+      dplyr::select(scenario, region, sources, param, class1, class2, units, x, value, vintage, xLabel, aggregate,
+                    classLabel1, classPalette1, classLabel2, classPalette2) %>%
+      unique()
 
     #---------------------
     # Save Data in CSV
     #---------------------
 
-    if (is.null(regions)) {
-        utils::write.csv(data, file = paste(dirOutputs, "/AllRegions/regional/dataTable_", min(range(data$x)),
-            "to", max(range(data$x)), ".csv", sep = ""), row.names = F)
-        utils::write.csv(dataTemplate, file = paste(dirOutputs, "/AllRegions/regional/dataTableTemplate_", min(range(data$x)),
-            "to", max(range(data$x)), ".csv", sep = ""), row.names = F)
+    if (is.null(regionsSelect)) {
+        utils::write.csv(datax, file = paste(dirOutputs, "/Tables_gcam/gcamDataTable_AllRegions_", min(range(datax$x)),
+            "to", max(range(datax$x)), ".csv", sep = ""), row.names = F)
+        utils::write.csv(dataTemplate, file = paste(dirOutputs, "/Tables_Template/template_Regional_AllRegions.csv", sep = ""),
+                         row.names = F)
     } else {
-        for (region_i in regions) {
-            utils::write.csv(data %>% dplyr::filter(region == region_i), file = paste(dirOutputs, "/", region_i,
-                "/regional/dataTable_",region_i,"_", min(range(data$x)), "to", max(range(data$x)), ".csv", sep = ""),
-                row.names = F)
-            utils::write.csv(dataTemplate %>% dplyr::filter(region == region_i), file = paste(dirOutputs, "/", region_i,
-                "/regional/dataTableTemplate_",region_i,"_", min(range(data$x)), "to", max(range(data$x)), ".csv", sep = ""),
-                row.names = F)
+
+       if(!all(regionsSelect %in% unique(datax$region))){
+          print(paste("Regions not available in data: ", paste(regionsSelect[!(regionsSelect %in% unique(datax$region))],collapse=", "), sep=""))
+          print(paste("Running remaining regions: ",  paste(regionsSelect[(regionsSelect %in% unique(datax$region))],collapse=", "), sep=""))
+       }
+
+        for (region_i in regionsSelect[(regionsSelect %in% unique(datax$region))]) {
+            utils::write.csv(datax %>% dplyr::filter(region == region_i),
+                             file = paste(dirOutputs, "/Tables_gcam/gcamDataTable_",region_i,"_", min(range(datax$x)),
+                                          "to", max(range(datax$x)), ".csv", sep = ""),row.names = F)
+            utils::write.csv(dataTemplate %>% dplyr::filter(region == region_i),
+                             file = paste(dirOutputs, "/Tables_Templates/template_Regional_",region_i,".csv", sep = ""),row.names = F)
+            utils::write.csv(dataTemplate %>% dplyr::filter(region == region_i),
+                             file = paste(dirOutputs, "/Tables_Local/local_Regional_",region_i,".csv", sep = ""),row.names = F)
         }
     }
 
-    return(list(data = data, dataTemplate = dataTemplate, scenarios = scenarios, queries = queries))
-
+    return(list(data = datax, dataTemplate = dataTemplate, scenarios = scenarios, queries = queries))
 
 }
