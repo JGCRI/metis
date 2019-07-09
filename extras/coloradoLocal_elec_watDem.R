22e96b8584c2088fe66f5ce3a5703ab8c0a908b7
FeralFlows <twild@umd.edu>
2019-07-09 16:13:29 -0400

    R scripts to produce base year calibration maps; masterX update to produce csv files to be used in mapping

---
+++ extras/coloradoLocal_elec_watDem.R
@@ -0,0 +1,130 @@
+
+#----------------------------
+# Install necessary packages
+#----------------------------
+if("devtools" %in% rownames(installed.packages()) == F){install.packages("devtools")}
+library(devtools)
+if("rgcam" %in% rownames(installed.packages()) == F){install_github(repo="JGCRI/rgcam")}
+library(rgcam)
+if("tibble" %in% rownames(installed.packages()) == F){install.packages("tibble")}
+library(tibble)
+if("dplyr" %in% rownames(installed.packages()) == F){install.packages("dlpyr")}
+library(dplyr)
+if("rgdal" %in% rownames(installed.packages()) == F){install.packages("rgdal")}
+library(rgdal)
+if("tmap" %in% rownames(installed.packages()) == F){install.packages("tmap")}
+library(tmap)
+if("rgeos" %in% rownames(installed.packages()) == F){install.packages("rgeos")}
+library(rgeos)
+if("tools" %in% rownames(installed.packages()) == F){install.packages("tools")}
+library(tools)
+if("metis" %in% rownames(installed.packages()) == F){install.packages("metis")}
+library(metis)
+
+setwd('C:/Users/twild/all_git_repositories/metis/metis')
+countryName="Argentina"
+
+
+# Plot 10 subregions in Colorado
+localBasinShapeFileFolder = paste(getwd(),"/dataFiles/gis/shapefiles_Argentina",sep="")
+localBasinShapeFile = "colorado_10_subregions"
+countryLocalBasin <-readOGR(dsn=localBasinShapeFileFolder,
+                            layer=localBasinShapeFile,use_iconv=T,encoding='UTF-8')
+localBasinsShapeFileColName = "cuenca" # Will need to load the file to see which name this would be
+countryLocalBasin<-countryLocalBasin[(!countryLocalBasin$cuenca %in%
+                                        c("media","baja","RioGrande","Barrancas")) & !is.na(countryLocalBasin$cuenca),]
+countryLocalBasin@data <- droplevels(countryLocalBasin@data)
+head(countryLocalBasin@data)
+plot(countryLocalBasin)
+writeOGR(obj=countryLocalBasin, dsn=paste(getwd(),"/dataFiles/gis/shapefiles_",countryName,sep=""), layer=paste(countryName,"LocalBasin",sep=""), driver="ESRI Shapefile", overwrite_layer=TRUE)
+metis.map(dataPolygon=countryLocalBasin,fillColumn = localBasinsShapeFileColName,printFig=F, facetsON = F, labels=F)
+
+
+
+
+subRegShpFolder_i = paste(getwd(),"/dataFiles/gis/shapefiles_",countryName,sep = "")
+subRegShpFile_i = paste("colorado_10_subregions",sep= "") # localBasinShapeFile # paste("colombiaLocalBasin",sep= "")
+subRegCol_i = localBasinsShapeFileColName  #
+subRegType_i = "subBasin"
+nameAppend_i = "_local"
+
+
+polygonDataTables_i=paste(getwd(),"/outputs/Maps/Tables/electricity_water_demand.csv",sep="")
+a1<-read.csv(polygonDataTables_i); head(a1); unique(a1$scenario); unique(a1$param); unique(a1$x)
+
+
+xRange_i= seq(from=2000,to=2050,by=5)
+legendPosition_i=c("LEFT","bottom")
+legendOutsideSingle_i=T
+animateOn_i=T
+delay_i=100
+scenRef_i="gfdl-esm2m_rcp2p6_NA_NA"
+paramsSelect_i = c("All")
+indvScenarios_i = "All"
+GCMRCPSSPPol_i=F
+
+scaleRange_i=data.frame(param=c("electricity_water_demand"),
+                        maxScale=c(5),
+                        minScale=c(0))
+numeric2Cat_param <- list("electricity_water_demand","param2")
+numeric2Cat_palette <- list(c("xx")) # Can be a custom scale or an R brewer paletter or a metis.pal
+numeric2Cat_legendTextSize <- list(c(0.7),c(NULL))
+numeric2Cat_breaks <- list(c(0, 0.25, 1, 3, 5),c(0,1,2,3,4,5))
+#numeric2Cat_labels <- list(c("None (0<WSI<0.1)","Low (0.1<WSI<0.2)","Moderate (0.2<WSI<0.4)","Severe (WSI>0.4)"),
+#                           c("a","b","c","d"))
+numeric2Cat_list <-list(numeric2Cat_param=numeric2Cat_param,
+                        numeric2Cat_breaks=numeric2Cat_breaks,
+                        numeric2Cat_palette=numeric2Cat_palette,
+                        numeric2Cat_legendTextSize=numeric2Cat_legendTextSize)
+
+list_index <- which(numeric2Cat_list$numeric2Cat_param=="electricity_water_demand")
+catBreaks <- numeric2Cat_list$numeric2Cat_breaks[[list_index]]; catBreaks
+#catLabels <- numeric2Cat_list$numeric2Cat_labels[[list_index]]; catLabels
+catPalette <- numeric2Cat_list$numeric2Cat_palette[[list_index]]; catPalette
+catLegendTextSize <- numeric2Cat_list$numeric2Cat_legendTextSize[[list_index]];catLegendTextSize
+
+boundaryRegShape_i = NULL
+boundaryRegShpFolder_i=paste(getwd(),"/dataFiles/gis/naturalEarth",sep="")
+boundaryRegShpFile_i=paste("ne_10m_admin_0_countries",sep="")
+boundaryRegCol_i="NAME"
+boundaryRegionsSelect_i=countryName
+
+# xRange=xRange_i,
+
+metis.mapProcess(polygonDataTables=polygonDataTables_i,
+                 #gridDataTables=gridDataTables_i,
+                 # boundaryRegShape=boundaryRegShape_i,
+                 # boundaryRegShpFolder=boundaryRegShpFolder_i,
+                 # boundaryRegShpFile=boundaryRegShpFile_i,
+                 # boundaryRegCol=boundaryRegCol_i,
+                 boundaryRegionsSelect=boundaryRegionsSelect_i,
+                 #subRegShape=subRegShape_i,
+                 subRegShpFolder=subRegShpFolder_i,
+                 subRegShpFile=subRegShpFile_i,
+                 subRegCol=subRegCol_i,
+                 subRegType=subRegType_i,
+                 nameAppend=nameAppend_i,
+                 legendOutsideSingle=legendOutsideSingle_i,
+                 legendPosition=legendPosition_i,
+                 animateOn=animateOn_i,
+                 delay=delay_i,
+                 scenRef=scenRef_i,
+                 extension=T,
+                 expandPercent = 3,
+                 figWidth=6,
+                 figHeight=7,
+                 paramsSelect = paramsSelect_i,
+                 scaleRange = scaleRange_i,
+                 indvScenarios=indvScenarios_i,
+                 GCMRCPSSPPol=GCMRCPSSPPol_i,
+                 multiFacetCols="scenarioRCP",
+                 multiFacetRows="scenarioGCM",
+                 legendOutsideMulti=T,
+                 legendPositionMulti=NULL,
+                 legendTitleSizeMulti=NULL,
+                 legendTextSizeAnim=NULL,
+                 legendTextSizeMulti=NULL,
+                 refGCM="gfdl-esm2m",
+                 refRCP="rcp2p6",
+                 chosenRefMeanYears=c(2000:2050),
+                 numeric2Cat_list=numeric2Cat_list)
--- extras/metis.masterX_io_colorado.R
+++ extras/metis.masterX_io_colorado.R
@@ -100,6 +100,7 @@ save_dir <- 'C:/Users/twild/all_git_repositories/metis/metis/outputs/Maps/Tables
 export_df <- mapping_df %>% filter(param %in% params) %>% select(-classPalette1)  # Deal with Palette separately in map
 write.csv(export_df, file=paste0(save_dir, '/', params, '.csv'), row.names=FALSE)
 #-----------------------------------------------------------------------------------------------------------------------
+
 # Store/export data related to runoff
 colnames <- c('scenario', 'region', 'subRegion', 'sources', 'param', 'units', 'class', 'x', 'value', 'subRegType',
               'classPalette1', 'classLabel1')
@@ -142,10 +143,76 @@ new_df_append['classLabel'] <- 'IrrigationDemand'
 for (subReg in subregions){
   df <- ioTable0 %>% filter(subRegion==subReg) %>% filter(grepl('W_', supplySubSector)) %>%
     select(-subRegion, -supplySector, -supplySubSector, -cap, -region, -units) %>%
-      select(contains("Ag_")) %>%
-      mutate(rowsum=rowSums(., na.rm=TRUE))
+    select(contains("Ag_")) %>%
+    mutate(rowsum=rowSums(., na.rm=TRUE))
   irrig_wat_dem_val <- sum((df)$rowsum, na.rm=TRUE)
   new_df_append <- new_df_append %>% mutate(value = if_else(subRegion==subReg, irrig_wat_dem_val, value))
+}
+mapping_df <- rbind(mapping_df, new_df_append)
+export_df <- mapping_df %>% filter(param %in% params)
+write.csv(export_df, file=paste0(save_dir, '/', params, '.csv'), row.names=FALSE)
+
+# Store/export data related to municipal water demand
+params <- c('municipal_water_demand')
+new_df_append['param'] <- params
+new_df_append['units'] <- 'km3'
+new_df_append['class'] <- 'Municipal'
+new_df_append['subRegType'] <- 'localBasin'
+new_df_append['x'] <- 2010
+new_df_append['value'] <- 0
+new_df_append['classPalette'] <- 'Blues'
+new_df_append['classLabel'] <- 'MunicipalDemand'
+for (subReg in subregions){
+  df <- ioTable0 %>% filter(subRegion==subReg) %>% filter(grepl('W_', supplySubSector)) %>%
+    select(-subRegion, -supplySector, -supplySubSector, -cap, -region, -units) %>%
+    select(contains("municipal")) %>%
+    mutate(rowsum=rowSums(., na.rm=TRUE))
+  municipal_water_demand <- sum((df)$rowsum, na.rm=TRUE)
+  new_df_append <- new_df_append %>% mutate(value = if_else(subRegion==subReg, municipal_water_demand, value))
+}
+mapping_df <- rbind(mapping_df, new_df_append)
+export_df <- mapping_df %>% filter(param %in% params)
+write.csv(export_df, file=paste0(save_dir, '/', params, '.csv'), row.names=FALSE)
+
+# Store/export data related to electricity water demand
+params <- c('electricity_water_demand')
+new_df_append['param'] <- params
+new_df_append['units'] <- 'km3'
+new_df_append['class'] <- 'Electricity'
+new_df_append['subRegType'] <- 'localBasin'
+new_df_append['x'] <- 2010
+new_df_append['value'] <- 0
+new_df_append['classPalette'] <- 'Blues'
+new_df_append['classLabel'] <- 'ElectricityDemand'
+for (subReg in subregions){
+  df <- ioTable0 %>% filter(subRegion==subReg) %>% filter(grepl('W_', supplySubSector)) %>%
+    select(-subRegion, -supplySector, -supplySubSector, -cap, -region, -units) %>%
+    select(contains("Electricity_")) %>%
+    mutate(rowsum=rowSums(., na.rm=TRUE))
+  electricity_water_demand <- sum((df)$rowsum, na.rm=TRUE)
+  new_df_append <- new_df_append %>% mutate(value = if_else(subRegion==subReg, electricity_water_demand, value))
+}
+mapping_df <- rbind(mapping_df, new_df_append)
+export_df <- mapping_df %>% filter(param %in% params)
+write.csv(export_df, file=paste0(save_dir, '/', params, '.csv'), row.names=FALSE)
+
+# Store/export data related to livestock water demand
+params <- c('livestock_water_demand')
+new_df_append['param'] <- params
+new_df_append['units'] <- 'km3'
+new_df_append['class'] <- 'Livestock'
+new_df_append['subRegType'] <- 'localBasin'
+new_df_append['x'] <- 2010
+new_df_append['value'] <- 0
+new_df_append['classPalette'] <- 'Blues'
+new_df_append['classLabel'] <- 'ElectricityDemand'
+for (subReg in subregions){
+  df <- ioTable0 %>% filter(subRegion==subReg) %>% filter(grepl('W_', supplySubSector)) %>%
+    select(-subRegion, -supplySector, -supplySubSector, -cap, -region, -units) %>%
+    select(contains("Livestock_")) %>%
+    mutate(rowsum=rowSums(., na.rm=TRUE))
+  livestock_water_demand <- sum((df)$rowsum, na.rm=TRUE)
+  new_df_append <- new_df_append %>% mutate(value = if_else(subRegion==subReg, livestock_water_demand, value))
 }
 mapping_df <- rbind(mapping_df, new_df_append)
 export_df <- mapping_df %>% filter(param %in% params)
