d879a738e7e13528a2171ca626ca832426facd89
FeralFlows <twild@umd.edu>
2019-09-12 10:57:22 -0400

    script that processes original data and excludes GW, fixes water demand by crop, etc.

--- extras/ColoradoDataPreparation.R
+++ extras/ColoradoDataPreparation.R
@@ -115,105 +115,98 @@ ColoradoWaterLand_Summary <- Merged_Wat_Land_Colorado %>%
   ungroup()
 
 # Eliminate Groundwater usage from both the policy and non-policy files. Redistribute to surface water supply.
-#for (scen in scenarios){
-#  if(scen=='Reference'){
-      data <- SupDem_data
-      data_NOGW <- SupDem_data_NOGW
-#  }else if(scen=='Policy'){
-#      data <- SupDem_data_policy
-#      data_NOGW <- SupDem_data_NOGW_policy
-#  }
-  for (sR in subRegions){
-    SupDem_GW <- data %>%
-      filter(subRegion==sR, supplySubSector=='W_GW_Reservoir')
-    DemCls <- as.character(unique(SupDem_GW$demandClass))
-    for(dC in DemCls){
-      SupDem_GW_DemCls <- SupDem_GW %>%
-        filter(demandClass==dC) # filter GW data for subregion just by demand classes
-      SupDem_GW_DemCls_Value <- SupDem_GW_DemCls$localDataSubdivided[1]  # Get GW value for this demand class and subregion
-      data_NOGW <- data_NOGW %>%
-        mutate(localDataSubdivided=if_else(subRegion==sR & demandClass==dC & supplySubSector=='W_SW_Upstream', localDataSubdivided+SupDem_GW_DemCls_Value, localDataSubdivided)) %>%
-        filter(!supplySubSector=='W_GW_Reservoir')
-    }
+data <- SupDem_data
+data_NOGW <- SupDem_data_NOGW
+for (sR in subRegions){
+  SupDem_GW <- data %>%
+    filter(subRegion==sR, supplySubSector=='W_GW_Reservoir')
+  DemCls <- as.character(unique(SupDem_GW$demandClass))
+  for(dC in DemCls){
+    SupDem_GW_DemCls <- SupDem_GW %>%
+      filter(demandClass==dC) # filter GW data for subregion just by demand classes
+    SupDem_GW_DemCls_Value <- SupDem_GW_DemCls$localDataSubdivided[1]  # Get GW value for this demand class and subregion
+    data_NOGW <- data_NOGW %>%
+      mutate(localDataSubdivided=if_else(subRegion==sR & demandClass==dC & supplySubSector=='W_SW_Upstream', localDataSubdivided+SupDem_GW_DemCls_Value, localDataSubdivided)) %>%
+      filter(!supplySubSector=='W_GW_Reservoir')
   }
-  # Replace dollar values with land values
-  profitLanduseMapping <- read.xlsx(profit_landuse_mapping_file, sheetName='agg crop by 10 subreg') %>%
-    rename(supplySubSector=crop) %>%
-    mutate(supplySubSector = paste0('Ag_', supplySubSector)) %>%
-    select(-gross.value..mil.pesos.) %>%
-    filter(!subRegion %in% 'SUM')
+}
+# Replace dollar values with land values
+profitLanduseMapping <- read.xlsx(profit_landuse_mapping_file, sheetName='agg crop by 10 subreg') %>%
+  rename(supplySubSector=crop) %>%
+  mutate(supplySubSector = paste0('Ag_', supplySubSector)) %>%
+  select(-gross.value..mil.pesos.) %>%
+  filter(!subRegion %in% 'SUM')
 
-  data_NOGW$units <- as.character(data_NOGW$units)
+data_NOGW$units <- as.character(data_NOGW$units)
 
-  data_NOGW <- data_NOGW %>%
-    left_join(profitLanduseMapping, by=c('subRegion', 'supplySubSector')) %>%
-    mutate(New_crop.area..km2.=if_else(is.na(crop.area..km2.), 0, crop.area..km2.)) %>%
-    mutate(localDataSubdivided=if_else(New_crop.area..km2. > 0, New_crop.area..km2., localDataSubdivided)) %>%
-    mutate(units=if_else(New_crop.area..km2. > 0, 'km2', units)) %>%
-    select(-crop.area..km2., -New_crop.area..km2., -localData) %>%
-    rename(localData = localDataSubdivided)
+data_NOGW <- data_NOGW %>%
+  left_join(profitLanduseMapping, by=c('subRegion', 'supplySubSector')) %>%
+  mutate(New_crop.area..km2.=if_else(is.na(crop.area..km2.), 0, crop.area..km2.)) %>%
+  mutate(localDataSubdivided=if_else(New_crop.area..km2. > 0, New_crop.area..km2., localDataSubdivided)) %>%
+  mutate(units=if_else(New_crop.area..km2. > 0, 'km2', units)) %>%
+  select(-crop.area..km2., -New_crop.area..km2., -localData) %>%
+  rename(localData = localDataSubdivided)
 
-  misc_crop_coeff <- (ColoradoWaterLand_Summary %>% filter(AggCrop=='MiscCrop'))$mean[1]
-  data_NOGW <- data_NOGW %>%
-    left_join(ColoradoWaterLand_Summary %>% rename(WatIntensCoeff=mean), by=c('demandClass'='AggCrop')) %>%
-    mutate(WatIntensCoeff=if_else(demandClass %in% c('Ag_specialty', 'Ag_vegetables', 'Ag_fruittrees'), misc_crop_coeff, WatIntensCoeff)) # insert coefficient for miscellaneous crops
+misc_crop_coeff <- (ColoradoWaterLand_Summary %>% filter(AggCrop=='MiscCrop'))$mean[1]
+data_NOGW <- data_NOGW %>%
+  left_join(ColoradoWaterLand_Summary %>% rename(WatIntensCoeff=mean), by=c('demandClass'='AggCrop')) %>%
+  mutate(WatIntensCoeff=if_else(demandClass %in% c('Ag_specialty', 'Ag_vegetables', 'Ag_fruittrees'), misc_crop_coeff, WatIntensCoeff)) # insert coefficient for miscellaneous crops
 
-  # Calculate the sum of the intensitity values (water per unit area) that occur within each region/subRegion
-  Subreg_Crop_Intensity <- data_NOGW %>%
-    filter(supplySector=='Water') %>%
-    mutate(delete = grepl('Ag_', demandClass)) %>%
-    filter(delete==TRUE) %>%
-    select(region, subRegion, demandClass, WatIntensCoeff)
+# Calculate the sum of the intensity values (water per unit area) that occur within each region/subRegion
+Subreg_Crop_Intensity <- data_NOGW %>%
+  filter(supplySector=='Water') %>%
+  mutate(delete = grepl('Ag_', demandClass)) %>%
+  filter(delete==TRUE) %>%
+  select(region, subRegion, demandClass, WatIntensCoeff)
 
-  # Join WIC values with land categories by supply subsector.
-  SubRegLand <- data_NOGW %>%
-    filter(supplySector=='Agriculture') %>%
-    select(region, subRegion, supplySubSector, localData) %>%
-    left_join(Subreg_Crop_Intensity %>% rename(supplySubSector = demandClass), by=c('region', 'subRegion', 'supplySubSector'))
+# Join WIC values with land categories by supply subsector.
+SubRegLand <- data_NOGW %>%
+  filter(supplySector=='Agriculture') %>%
+  select(region, subRegion, supplySubSector, localData) %>%
+  left_join(Subreg_Crop_Intensity %>% rename(supplySubSector = demandClass), by=c('region', 'subRegion', 'supplySubSector'))
 
-  # Eliminates NA values for WIC (e.g., for Forest)
-  SubRegLand <- SubRegLand %>%
-    mutate(WatIntensCoeff = replace(WatIntensCoeff, which(is.na(WatIntensCoeff)), 0)) %>%
-    mutate(Estim_TotBiophysWatDem = localData*WatIntensCoeff)
+# Eliminates NA values for WIC (e.g., for Forest)
+SubRegLand <- SubRegLand %>%
+  mutate(WatIntensCoeff = replace(WatIntensCoeff, which(is.na(WatIntensCoeff)), 0)) %>%
+  mutate(Estim_TotBiophysWatDem = localData*WatIntensCoeff)
 
-  # Calculate total biophysical estiomated water requirement for each subregion-Agg_crop category.
-  TotalBiophys_SubRegLand <- SubRegLand %>%
-    group_by(region, subRegion) %>%
-    summarize(Total_subreg_Biophys=sum(Estim_TotBiophysWatDem)) %>%
-    ungroup()
+# Calculate total biophysical estiomated water requirement for each subregion-Agg_crop category.
+TotalBiophys_SubRegLand <- SubRegLand %>%
+  group_by(region, subRegion) %>%
+  summarize(Total_subreg_Biophys=sum(Estim_TotBiophysWatDem)) %>%
+  ungroup()
 
-  # Merge in the above back into the SubRegLand and calculate fractions of water that should go to each Agg_crop-subregion category
-  SubRegLand <- SubRegLand %>%
-    left_join(TotalBiophys_SubRegLand, by=c('region', 'subRegion')) %>%
-    mutate(fraction_water = Estim_TotBiophysWatDem / Total_subreg_Biophys)
+# Merge in the above back into the SubRegLand and calculate fractions of water that should go to each Agg_crop-subregion category
+SubRegLand <- SubRegLand %>%
+  left_join(TotalBiophys_SubRegLand, by=c('region', 'subRegion')) %>%
+  mutate(fraction_water = Estim_TotBiophysWatDem / Total_subreg_Biophys)
 
-  # Calculate the sum of water demanded within each region/subRegion. This water will be allocated using fractions calculated above.
-  sumSubregWatDem <- data_NOGW %>%
-    filter(supplySector=='Water') %>%
-    mutate(delete = grepl('Ag_', demandClass)) %>%
-    filter(delete==TRUE) %>%
-    group_by(region, subRegion) %>%
-    summarize(TotalWatDem=sum(localData))
+# Calculate the sum of water demanded within each region/subRegion. This water will be allocated using fractions calculated above.
+sumSubregWatDem <- data_NOGW %>%
+  filter(supplySector=='Water') %>%
+  mutate(delete = grepl('Ag_', demandClass)) %>%
+  filter(delete==TRUE) %>%
+  group_by(region, subRegion) %>%
+  summarize(TotalWatDem=sum(localData))
 
-  # Merge in the total water demand sum for each subregion into the main dataframe, and
-  # merge fractions into dataframe
-  data_NOGW <- data_NOGW %>%
-    left_join(sumSubregWatDem, by=c('region', 'subRegion')) %>%
-    left_join(SubRegLand %>%
-                rename(demandClass=supplySubSector) %>%
-                select(region, subRegion, demandClass, fraction_water), by=c('region', 'subRegion', 'demandClass')) %>%
-    mutate(new_localData=localData) %>%  # preserve original values for non-crop water demand sectors
-    mutate(new_localData=if_else(is.na(fraction_water)==FALSE, fraction_water*TotalWatDem, new_localData)) %>%
-    select(-localData) %>%
-    rename(localData = new_localData)
+# Merge in the total water demand sum for each subregion into the main dataframe, and
+# merge fractions into dataframe
+data_NOGW <- data_NOGW %>%
+  left_join(sumSubregWatDem, by=c('region', 'subRegion')) %>%
+  left_join(SubRegLand %>%
+              rename(demandClass=supplySubSector) %>%
+              select(region, subRegion, demandClass, fraction_water), by=c('region', 'subRegion', 'demandClass')) %>%
+  mutate(new_localData=localData) %>%  # preserve original values for non-crop water demand sectors
+  mutate(new_localData=if_else(is.na(fraction_water)==FALSE, fraction_water*TotalWatDem, new_localData)) %>%
+  select(-localData) %>%
+  rename(localData = new_localData)
 
-  # Get rid of extra unneeded columns
-  data_NOGW <- data_NOGW %>%
-    select(-WatIntensCoeff, -TotalWatDem, -fraction_water)
+# Get rid of extra unneeded columns
+data_NOGW <- data_NOGW %>%
+  select(-WatIntensCoeff, -TotalWatDem, -fraction_water)
 
-  # Save modified file
-  write.csv(data_NOGW, file=paste0(save_dir, '/', 'colorado', '_', scen, '_NEW', '.csv'), row.names=FALSE)
-#}
+# Save modified file
+write.csv(data_NOGW, file=paste0(save_dir, '/', 'colorado', '_', 'Reference', '_NEW', '.csv'), row.names=FALSE)
 
 # Prepare dataframe for policy
 policy_land_multiplier = list('RioNegro_baja' = 0.835, 'LaPampa_baja' = 127.083)
@@ -245,43 +238,45 @@ plot_DF <- plot_DF %>%
     mutate(delete = grepl('Ag_', supplySubSector)) %>%
     filter(delete==TRUE) %>%
     select(-delete) %>%
-    mutate(supplySubSector=if_else(supplySubSector=='Ag_cereals', 'Cereals (Wheat, Corn, etc.)', supplySubSector)) %>%
-    mutate(supplySubSector=if_else(supplySubSector=='Ag_fruittrees', 'Fruit Trees (Apples, Pears, etc.)', supplySubSector)) %>%
-    mutate(supplySubSector=if_else(supplySubSector=='Ag_vegetables', 'Vegetables (Onions, Pumpkins, etc.)', supplySubSector)) %>%
+    mutate(supplySubSector=if_else(supplySubSector=='Ag_cereals', 'Cereals', supplySubSector)) %>%
+    mutate(supplySubSector=if_else(supplySubSector=='Ag_fruittrees', 'Fruit Trees', supplySubSector)) %>%
+    mutate(supplySubSector=if_else(supplySubSector=='Ag_vegetables', 'Vegetables', supplySubSector)) %>%
     mutate(supplySubSector=if_else(supplySubSector=='Ag_pasture', 'Pasture', supplySubSector)) %>%
-    mutate(supplySubSector=if_else(supplySubSector=='Ag_specialty', 'Specialty (Wine, Olives, etc.)', supplySubSector)) %>%
-    rename(crop=supplySubSector) %>%
+    mutate(supplySubSector=if_else(supplySubSector=='Ag_specialty', 'Specialty', supplySubSector)) %>%
+    rename(Crop=supplySubSector) %>%
     rename(FillLabel=units)
 
-
-CropPalette <- c(Pasture = "#a6cdd9", `Specialty (Wine, Olives, etc.)` = "#d2e4ee",
-                 `Vegetables (Onions, Pumpkins, etc.)` = "#8bb086",
-                 `Cereals (Wheat, Corn, etc.)` = "#b7b079",
-                 `Fruit Trees (Apples, Pears, etc.)` = "#efc750")
+CropPalette <- c(`Pasture` = "#a6cdd9",
+                 `Specialty` = "#d2e4ee",
+                 `Vegetables` = "#8bb086",
+                 `Cereals` = "#b7b079",
+                 `Fruit Trees` = "#efc750")
 
 plot_DF$FillPalette <- 'CropPalette'
 base_fig_path <- 'C:/Users/twild/Dropbox/Argentina and metis workplan/Metis_Colorado/paper/figures'
-x_labels <-list('RioNegro_baja' = c("Cereal", "Fruit Trees", "Pasture", "Vegetables"),
-                'LaPampa_baja' = c("Pasture", "Specialty", "Vegetables"))
+#x_labels <-list('RioNegro_baja' = c("Cereal", "Fruit Trees", "Pasture", "Vegetables"),
+#                'LaPampa_baja' = c("Pasture", "Specialty", "Vegetables"))
 label_names <- c("Reference" = "Reference Scenario",
                 "Policy" = "Irrigation Expansion")
 sR_array <- c('RioNegro_baja', 'LaPampa_baja')
+
 for(sR in sR_array){
   plot_DF_sR <- plot_DF %>%
     filter(subRegion == sR) %>%
     mutate(FacetVar = scenario)
-  plot_DF_sR$crop <- as.factor(plot_DF_sR$crop)
+  #plot_DF_sR$FacetVar <- as.factor(plot_DF_sR$FacetVar, levels=c("Reference","Policy"))
+  plot_DF_sR$FacetVar = factor(plot_DF_sR$FacetVar, levels = c('Reference', 'Policy'))
+  plot_DF_sR$Crop <- as.factor(plot_DF_sR$Crop)
   paletteX<-get(plot_DF_sR$FillPalette)
-  p <- ggplot(plot_DF_sR, aes( x = crop, y = localData, fill = crop) ) +  # group=scenario,
+
+  p <- ggplot(plot_DF_sR, aes( x = Crop, y = localData, fill = Crop) ) +
     geom_bar( stat = "identity", width = 0.75, color = "#2b2b2b", size = 0.05 ) +
-  #  scale_y_continuous( labels = percent, limits = c( 0, 0.5 ) ) +
-    scale_x_discrete( expand = c( 0, 1 ), labels = x_labels[[sR]] ) +
-    scale_fill_manual( values = paletteX ) +  # ,    c("#efc750", "#a6cdd9", "#d2e4ee", "#b7b079")
+    scale_fill_manual( values = paletteX ) +
     facet_wrap( ~ FacetVar, labeller = as_labeller(label_names) ) +
-    #facet_grid(.~FacetVar, drop=TRUE, space="free", scales="free") +
-  #  labs( x = NULL, y = NULL, title = "Reference vs. Policy", element_text(hjust=0.5) ) +
     ylab(expression(Land~Allocation~by~Crop~(km^2))) +
-    theme( strip.text = element_text( size = 12, color = "white", hjust = 0.5 ),
+    guides(fill=guide_legend()) +
+    ggtitle(paste0('Sub-region: ', sR)) +
+    theme( strip.text = element_text( size = 10, color = "white", hjust = 0.5 ),
            strip.background = element_rect( fill = "#858585", color = NA ),
            panel.background = element_rect( fill = "#efefef", color = NA ),
            panel.grid.major.x = element_blank(),
@@ -290,7 +285,23 @@ for(sR in sR_array){
            panel.grid.major.y = element_line( color = "#b2b2b2" ),
            panel.spacing.x = unit( 1, "cm" ),
            panel.spacing.y = unit( 0.5, "cm" ),
-           legend.position = "none" )
+           legend.position = "bottom",
+           text =                element_text(family = NULL, face = "plain",colour = "black", size = 10 ,hjust = 0.5, vjust = 0.5, angle = 0, lineheight = 0.9)
+           , axis.text.x =       element_text(size=8)
+           , axis.text.y =       element_text(size=10)
+           ,axis.title.x =       element_text(vjust = -1, margin=margin(t=1,unit="line"))
+           ,axis.title.y =       element_text(angle = 90, vjust = 2, margin=margin(r=1,unit="line"))
+#           ,legend.key =         element_blank()
+#           ,legend.key.size =    unit(1.5, 'lines')
+           ,legend.text =        element_text(size = 10, colour = "black")
+#           ,legend.title =       element_text(size = rel(1.2), face = NULL, hjust = 0, colour = "black")
+           ,legend.title = element_blank()
+#           ,strip.background =   element_rect(fill = NA, colour = "black")
+#           ,plot.margin =        unit(c(1, 1, 1, 1), "lines")
+           ,plot.title=          element_text(face="bold", hjust=0.5,size=10,margin = margin(b=20))
+           ,legend.spacing.x = unit(0.5, 'cm')
+           ,legend.background = element_rect(linetype = 1, size = 0.5, colour = 1)
+           )
   p
-  ggsave(paste0(base_fig_path, '/', 'ColoradoAg', '_', sR, ".png"), plot=p, dpi=300, width=10, height=5, units="in")  # Save image
+  ggsave(paste0(base_fig_path, '/', 'ColoradoAg', '_', sR, ".pdf"), plot=p, dpi=1200, width=6, height=3.5, units="in")  # Save image
 }
